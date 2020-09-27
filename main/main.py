9d8c6d2e7ac49c5a4a74f0d19d30e876a3f8050e
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2020-09-28 01:00:21 +0530

    Fix orientation and margin bug

--- main/main.py
+++ main/main.py
@@ -1,21 +1,24 @@
 import glob
 import os
+import time
 from time import sleep
+
+from docx.api import Document
 try:
     import docx
-    import PyPDF2
 except ModuleNotFoundError:
     print("Required dependencies not found!!")
     ip = input("Do you wish to install them?[y/n]")
     if(ip == "y" or ip == "Y"):
         os.system("pip install python-docx")
-        os.system("pip install PyPDF2")
+    else:
+        exit()
 
 
-def getResultantFilename(s="New File", ext=""):
+def getResultantFilename(s="New File", ext="docx"):
     temp = s[:]
     num = 1
-    while(os.path.isfile(os.getcwd()+"/../OUTPUT/"+temp+".pdf")):
+    while(os.path.isfile(os.getcwd()+"/../OUTPUT/"+temp+"."+ext)):
         temp = s[:]
         temp += "[{}]".format(num)
         num += 1
@@ -24,12 +27,25 @@ def getResultantFilename(s="New File", ext=""):
     return temp
 
 
+def setOrientation(document):
+    curr_section = document.sections[-1]
+    curr_section.left_margin = docx.shared.Inches(0.2)
+    curr_section.right_margin = docx.shared.Inches(0.2)
+    curr_section.top_margin = docx.shared.Inches(0.2)
+    curr_section.bottom_margin = docx.shared.Inches(0.2)
+    curr_section.orientation = docx.enum.section.WD_ORIENT.LANDSCAPE
+    curr_section.page_width, curr_section.page_height = curr_section.page_height, curr_section.page_width
+    img_width = curr_section.page_width-2*curr_section.left_margin
+    img_height = curr_section.page_height-2*curr_section.bottom_margin
+    return document, img_width, img_height
+
+
 def imageToPDFs(imageArr, filename):
     document = docx.Document()
+    document, img_width, img_height = setOrientation(document)
     for i in imageArr:
-        document.add_picture(i, width=docx.shared.Inches(6.5))
-        document.add_page_break()
-    document.save(filename)
+        document.add_picture(i, width=img_width, height=img_height)
+    document.save("../OUTPUT/"+filename)
 
 
 def getImgArray():
@@ -37,7 +53,7 @@ def getImgArray():
     ImgArray = []
     temp.append(glob.glob('../DROP/*.jpg'))
     temp.append(glob.glob('../DROP/*.png'))
-    temp.append(glob.glob('../DROP/*.jprg'))
+    temp.append(glob.glob('../DROP/*.jpeg'))
     for i in temp:
         for j in i:
             ImgArray.append(j)
@@ -56,7 +72,6 @@ def main():
         print(chr(i+97), ":", j.split("\\")[1])
         ImgDict[chr(i+97)] = j
     ip = input("Enter required order [#:default]: ").lower().strip().split()
-    print(ip)
     if(ip[0] == "#"):
         newImgArray = ImgArray[:]
     else:
@@ -66,14 +81,14 @@ def main():
             except Exception:
                 print("Invalid values!!")
                 exit()
-    print(newImgArray)
-    # filename = getResultantFilename("test", "docx")
-    # imageToPDFs(newImgArray, filename)
+    filename = input("Enter name of file(without extension):")
+    filename = getResultantFilename(filename, "docx")
+    imageToPDFs(newImgArray, filename)
+    print("DONE!")
+    time.sleep(1)
+    print("Goodbye!")
+    time.sleep(1)
 
 
 if __name__ == "__main__":
-    # print(getImgArray())
-    # main()
-    imageArray = ['../DROP\\Screenshot (62).png', '../DROP\\Screenshot (63).png',
-                  '../DROP\\Screenshot (64).png', '../DROP\\Screenshot (65).png', '../DROP\\Screenshot (66).png']
-    imageToPDFs(imageArray, "test.docx")
+    main()
